773d7236ae3d7c69ff25c95950e9a92bd0ca4340
Sung-Hoon Kim <sungspot@gmail.com>
2017-03-09 21:51:27 -0500

    update animation

--- course/js/grid_module.js
+++ course/js/grid_module.js
@@ -7,11 +7,12 @@
 
 define(['jquery','vivus'], function ($,vivus) {
 
-    const OUTLINE_DUR = 100;
-    const COL_DUR = 500;
-    const ROW_DUR = 50;
-    const COL_DELAY = 10;
-    const ROW_DELAY = 20;
+    const OUTLINE_DUR = 20;
+    const COL_DUR = 20;
+    const ROW_DUR = 20;
+    const OUTLINE_DELAY = 7;
+    const COL_DELAY = 0;
+    const ROW_DELAY = 0;
     const COLOR_DUR = 400;
     const COLOR_DELAY = 100;
     // throw a generic error
@@ -26,7 +27,7 @@ define(['jquery','vivus'], function ($,vivus) {
         var rows = data.rows;
         var w = data.colwidth;
         var h = data.rowheight;
-        var rg = data.rowgap;
+        var rg = data.rowgap;   // TODO
         var cg = data.colgap;
         var p = data.hpadding;
         var innerw = columns * (w + cg) - cg;
@@ -44,63 +45,84 @@ define(['jquery','vivus'], function ($,vivus) {
           'max-height' : 95 + 'vh',
           'max-width' : rat * 95 + 'vh'
         });
-        //
-        $youshi.append($('<path class="grid-outline" data-delay="7" data-async="" d="M'+(fullw)+',0 h'+(-1*fullw)+'" />').attr('data-duration',OUTLINE_DUR));
-        // bottom
-        $youshi.append($('<path class="grid-outline" data-async="" d="M0,'+maxh+' h'+fullw+'" />').attr('data-duration',OUTLINE_DUR))
-        // right
-        $youshi.append($('<path class="grid-outline" data-async="" d="M'+(fullw)+','+maxh+' v'+(-1*maxh)+'" />').attr('data-duration',OUTLINE_DUR))
-        // left
-        $youshi.append($('<path class="grid-outline" d="M 0,0 v'+maxh+'" />').attr('data-duration',OUTLINE_DUR))
-        var pathstart = '<path class="grid" d=" ';
-        var pathend = '" />';
-        var allcol = []
-        var allrow = []
-
-        for (var i = 0; i < columns; i++) {
-
-          var colpath = pathstart;
-          colpath += 'M'+ (maxw - (i*(w+cg))) +',0 v' + maxh + ' ';
-          // $youshi.append($('<path d="M '+ (maxw - (i*(w+cg))) +' 0 v ' + maxh + '" fill="none" stroke="gray" stroke-width="1"/>'))
-          if (cg > 0) {
-            colpath += 'M'+ (maxw - w - (i*(w + cg))) +',0 v' + maxh + ' '
-            // $youshi.append($('<path d="M '+ (maxw - w - (i*(w + cg))) +' 0 v ' + maxh + '" fill="none" stroke="gray" stroke-width="1"/>'))
-          }
-
-          colpath += pathend;
-          // allcol.push(colpath);
-
-          $youshi.append($(colpath).addClass('.grid-vertical')
-            .attr({
-              'data-async':'',
-              'data-delay':COL_DELAY,
-              'data-duration':COL_DUR
-            })
-          );
-        }
-        for (var j = 0; j < rows + 1; j++) {
-          var rowpath = pathstart;
-          rowpath += 'M' + (maxw - (i*(w+cg))) +','+ (j * h) + ' h' + (-1 * w) + ' '
-          // $youshi.append($('<path d="M ' + (maxw - (i*(w+cg))) +' '+ (j * h) + ' h ' + (-1 * w) + '" fill="none" stroke="gray" stroke-width="1"/>'))
-
-          rowpath += pathend;
-          // allrow.push(rowpath);
-        }
-
-        $youshi.append($(rowpath).addClass('.grid-horizontal')
+        // top
+        $youshi.append($('<path d="M'+(fullw)+',0 h'+(-1*fullw)+'" />').addClass('grid-outline')
           .attr({
-            'data-async':'',
-            'data-delay':ROW_DELAY,
-            'data-duration':ROW_DUR
+            'data-async' : '',
+            'data-delay' : OUTLINE_DELAY,
+            'data-duration' : OUTLINE_DUR
           })
         );
-        // allcol.forEach(function(colpath) {
-        //   $youshi.append($(colpath).attr('data-async','').attr('data-delay',1).attr('data-duration',50));
-        // });
-        // allrow.forEach(function(rowpath) {
-        //   $youshi.append($(rowpath));
-        // })
+        // bottom
+        $youshi.append($('<path d="M0,'+maxh+' h'+fullw+'" />').addClass('grid-outline')
+          .attr({
+            'data-async' : '',
+            'data-duration' : OUTLINE_DUR
+          })
+        );
+        // right
+        $youshi.append($('<path d="M'+(fullw)+','+maxh+' v'+(-1*maxh)+'" />').addClass('grid-outline')
+          .attr({
+            'data-async' : '',
+            'data-duration' : OUTLINE_DUR
+          })
+        );
+        // left
+        $youshi.append($('<path d="M 0,0 v'+maxh+'" />').addClass('grid-outline')
+          .attr({
+            'data-async' : '',
+            'data-duration' : OUTLINE_DUR
+          })
+        );
+        var pathstart = '<path class="grid" d=" ';
+        var pathend = '" />';
 
+        for (var i = 0; i < columns; i++) {
+          var colpath = pathstart;
+          var pill = [];
+          var x = (maxw - (i*(w+cg)));
+          var y,dy;
+          if (i%2==0) {
+            y = 0;
+            dy = maxh;
+          } else {
+            y = maxh;
+            dy = -1 * maxh;
+          }
+          colpath += 'M'+ x +','+y+' v' + dy + ' ';
+          colpath += pathend;
+          pill.push(colpath);
+          if (cg > 0) {
+            colpath = pathstart;
+            colpath += 'M'+ (x-w) +','+y+' v' + dy + ' ';
+            colpath += pathend;
+            pill.push(colpath);
+          }
+          pill.forEach(function(temppath) {
+            $youshi.append($(temppath).addClass('grid-vertical')
+              .attr({
+                'data-async':'',
+                'data-delay':COL_DELAY,
+                'data-duration':COL_DUR
+              })
+            );
+          })
+        }
+        for (var j = 1; j < rows; j++) {
+          for (var i = 0; i < columns; i++) {
+            var rowpath = pathstart;
+            // rowpath += 'M' + (maxw) +','+ (j * h) + ' h' + (-1 * innerw) + ' ';
+            rowpath += 'M' + (maxw - (i*(w+cg))) +','+ (j * h) + ' h' + (-1 * w) + ' ';
+            rowpath += pathend;
+            $youshi.append($(rowpath).addClass('grid-horizontal')
+              .attr({
+                'data-async':'',
+                'data-delay':ROW_DELAY,
+                'data-duration':ROW_DUR
+              })
+            );
+          }
+        }
 
         // have to refresh the div
         var $cont = $('#svgcont');
@@ -124,9 +146,6 @@ define(['jquery','vivus'], function ($,vivus) {
 
         $('.loading').removeClass('loading');
         new vivus('youshi', {delay: 0, duration: 5, type: 'scenario-sync'},function() {
-          $('.grid-horizontal').animate({
-            'stroke-dasharray' : w+','+cg
-          })
           $('.youshi-col-full').each(function(i,el) {
             setTimeout(function() {
               $(el).mouseenter();
